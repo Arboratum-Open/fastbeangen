ef8882887552c1dc566d36e620e8479af294a081
Geoffrey Picron <geoffrey.picron@arboratum.com>
2016-10-18 06:21:19 +0200

    added single country generato builder

--- src/main/java/com/arboratum/beangen/extended/IdentityGeneratorBuilder.java
+++ src/main/java/com/arboratum/beangen/extended/IdentityGeneratorBuilder.java
@@ -17,12 +17,17 @@ public class IdentityGeneratorBuilder extends AbstractGeneratorBuilder<Identity>
     public static final ImmutableSet<Class> SUPPORTED_TYPES = ImmutableSet.of(Identity.class);
 
     private static final Generator<CountryConfig> COUNTRY_CONFIG_GENERATOR = BaseBuilders.enumerated(CountryConfig.class)
-            .valuesFromCSVResource("/generator/country-population.csv", k -> CountryConfig.builder()
-                    .country(k)
-                    .familyName(BaseBuilders.enumerated(String.class).valuesFromCSVResource("/generator/familyName_" + k + ".csv").build())
-                    .givenNameMale(BaseBuilders.enumerated(String.class).valuesFromCSVResource("/generator/givenName_male_" + k + ".csv").build())
-                    .givenNameFemale(BaseBuilders.enumerated(String.class).valuesFromCSVResource("/generator/givenName_female_" + k + ".csv").build())
-                    .build()).build();
+            .valuesFromCSVResource("/generator/country-population.csv", k -> buildCountryConfig(k)).build();
+
+    private static CountryConfig buildCountryConfig(String iso2) {
+        return CountryConfig.builder()
+                .country(iso2)
+                .familyName(BaseBuilders.enumerated(String.class).valuesFromCSVResource("/generator/familyName_" + iso2 + ".csv").build())
+                .givenNameMale(BaseBuilders.enumerated(String.class).valuesFromCSVResource("/generator/givenName_male_" + iso2 + ".csv").build())
+                .givenNameFemale(BaseBuilders.enumerated(String.class).valuesFromCSVResource("/generator/givenName_female_" + iso2 + ".csv").build())
+                .build();
+    }
+
     private static final Generator<Identity.Gender> GENDER = BaseBuilders.anEnum(Identity.Gender.class).uniform().build();
     private static final Generator<java.util.Date> DATE_OF_BIRTH = BaseBuilders.aDate().uniform(
             Date.from(Instant.parse("1926-01-01T00:00:00Z")),Date.from(Instant.parse("2016-01-01T00:00:00Z"))).build();
@@ -40,7 +45,26 @@ public class IdentityGeneratorBuilder extends AbstractGeneratorBuilder<Identity>
         private final Generator<String> familyName;
     }
 
+    public IdentityGeneratorBuilder country(String iso) {
+        final CountryConfig countryOfBirth = buildCountryConfig(iso);
+        setup(seq -> {
+            final Identity.Gender gender = GENDER.generate(seq);
 
+            Identity identity = new Identity();
+            identity.setDateOfBirth(DATE_OF_BIRTH.generate(seq));
+            identity.setGender(gender);
+            identity.setCountryOfBirth(countryOfBirth.getCountry());
+            if (gender == Identity.Gender.male) {
+                identity.setFirstNames(countryOfBirth.getGivenNameMale().generate(seq));
+            } else {
+                identity.setFirstNames(countryOfBirth.getGivenNameFemale().generate(seq));
+            }
+            identity.setFamilyNames(countryOfBirth.getFamilyName().generate(seq));
+
+            return identity;
+        });
+
+    }
 
     public IdentityGeneratorBuilder all() {
 
